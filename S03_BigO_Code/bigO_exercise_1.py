02316f97c53ba03fde6ef5fa19a8d924aae9178f
Tibor Hajszan <thajszan@t-online.hu>
2024-10-31 15:51:24 +0100

    Lesson "Solution: Big O Calculation": Completed.

--- S03_BigO_Code/bigO_exercise_1.py
+++ S03_BigO_Code/bigO_exercise_1.py
@@ -1,14 +1,16 @@
 
-### What is the Big O of the function below? (Hint, you may want to go line by line)
+### What is the Big O of the function below? Hint: You may want to go line by line...
 
 def funChallenge(input):
   
-    a = 10
-    a = 50 + 3
+    a = 10 # O(1)
+    a = 50 + 3 # O(1)
 
-    for item in input:
-        anotherFunction()
-        stranger = True
-        a += 1
+    for item in input: # O(n)
+        anotherFunction() # O(n)
+        stranger = True # O(n)
+        a += 1 # O(n)
     
-    return a
+    return a # O(1)
+
+### Big O = 1 + 1 + n + n + n + n + 1 = 3 + 4n = O(4n+3) >>> O(n)

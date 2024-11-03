f646faa0af3e44492fb8a5732b2391cd3a562661
Tibor Hajszan <thajszan@t-online.hu>
2024-11-03 15:45:56 +0100

    Lesson "Exercise: Space Complexity" : Completed.

--- S03_BigO_Code/space_complexity.py
+++ S03_BigO_Code/space_complexity.py
@@ -4,12 +4,15 @@ def boooo(n):
     for _ in range(n):
         print("booooo")
 
+boooo(5)
+### Big sO = 0 = O(0) >>> O(1)
+
 ### -6- space complexity O(n)
 def hi_ntimes(n):
-    hi_array = []
+    hi_array = [] # sO(1)
     for _ in range(n):
-        hi_array.append("hi")
+        hi_array.append("hi") # sO(n)
     return hi_array
 
-boooo(5)
 print(hi_ntimes(6))
+### Big sO = 1 + n = O(n+1) >>> O(n)

0434a79400724c843d99dd6e324e8084b94a8efd
Tibor Hajszan <thajszan@t-online.hu>
2025-01-06 15:00:23 +0100

    Lesson "Solution: append()" : Completed.

--- S08_LinkedLists/linkedlist_implementation.py
+++ S08_LinkedLists/linkedlist_implementation.py
@@ -14,10 +14,10 @@
 class LinkedList:
 
     ### constructor method #############################################################################################
-    def __init__(self, value=None) -> None:
+    def __init__(self, headValue=None) -> None:
         """
         Args:
-        - value : Any, value of head node, defaults to None
+        - headValue : Any, value of head node, defaults to None
 
         Initializes a new LinkedList object with a single head node.
 
@@ -30,14 +30,34 @@ class LinkedList:
         - None
         """
 
-        self.head = {"value": value, "next": None}
+        self.head = {"value": headValue, "next": None}
         self.tail = self.head
         self.length = int(1)
+        return
+    
+    ### append method ##################################################################################################
+    def append(self, appendValue=None) -> None:
+        """
+        Args:
+        - appendValue : Any, value of new node, defaults to None
+
+        Adds a new node to the end of the linked list.
+        
+        Returns:
+        - None
+        """
+
+        new_node = {"value": appendValue, "next": None}
+        self.tail["next"] = new_node
+        self.tail = new_node
+        self.length += 1
         return
 
 ### testing code #######################################################################################################
 
-myLiLi = LinkedList(value=10)
+myLiLi = LinkedList(headValue=10)
+myLiLi.append(appendValue=5)
+myLiLi.append(appendValue=16)
 print()
 print("Head: ", myLiLi.head, "\n")
 print("Tail: ", myLiLi.tail, "\n")

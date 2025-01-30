95ef76bcf530bb3535a6b90c0c131d20aae4ba1b
Tibor Hajszan <thajszan@t-online.hu>
2025-01-30 14:45:58 +0100

    Lesson "Solution: Graph Implementation" : Coded __str__() method.

--- S11_Graphs/graph.py
+++ S11_Graphs/graph.py
@@ -13,7 +13,7 @@ from typing import Dict, Any
 
 class Graph:
 
-    ### constructor method ---------------------------------------------------------------------------------------------
+    ### constructor method #############################################################################################
     def __init__(self):
         """
         Initializes a new empty Graph object.
@@ -32,6 +32,36 @@ class Graph:
         self.nodes: Dict[Any,Any] = dict()
         self.size: int = int(0)
         return
+    
+    ### str dunder method ##############################################################################################
+    def __str__(self) -> str:
+        """
+        Defines the string representation for the Graph class.
+
+        Args:
+        - None
+
+        Returns:
+        - graph_string : str, string representation
+        """
+
+        ### initializing return string ---------------------------------------------------------------------------------
+
+        graph_string: str = str()
+
+        ### empty graph > constructing empty graph message -------------------------------------------------------------
+
+        if self.size == 0: graph_string = "Empty Graph" + "\n"
+
+        ### graph not empty > constructing string representation of graph ----------------------------------------------
+
+        else:
+            for key,value in self.nodes.items():
+                graph_string += f"{key} -> {value}\n"
+        
+        ### returning string -------------------------------------------------------------------------------------------
+
+        return graph_string
 
 ########################################################################################################################
 ### testing code
@@ -39,3 +69,4 @@ class Graph:
 
 print()
 my_graph = Graph()
+print("Init:", my_graph)

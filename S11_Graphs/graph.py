2ab471bada18ede2051b00680827740fa95ce656
Tibor Hajszan <thajszan@t-online.hu>
2025-01-30 15:57:19 +0100

    Lesson "Solution: Graph Implementation" : Coded add_node() method.

--- S11_Graphs/graph.py
+++ S11_Graphs/graph.py
@@ -1,6 +1,6 @@
 ########################################################################################################################
 ### Data Structures and Algorithms :: Section 11
-### Graph Implementation
+### Graph Implementation Using Dictionary as Adjacency / Node List
 ########################################################################################################################
 
 ### imports ------------------------------------------------------------------------------------------------------------
@@ -58,10 +58,33 @@ class Graph:
         else:
             for key,value in self.nodes.items():
                 graph_string += f"{key} -> {value}\n"
+            graph_string += f"Size: {self.size}\n"
         
         ### returning string -------------------------------------------------------------------------------------------
 
         return graph_string
+    
+    ### add node method ################################################################################################
+    def add_node(self, nodeValue:Any=None) -> bool:
+        """
+        Adds a new node containing the specified value to the Graph object.
+
+        Args:
+        - nodeValue : Any | None, value of node to be added, defaults to None
+
+        Returns:
+        - bool, True = node is added | False = node is not added
+        """
+
+        ### node already exists > returning false ----------------------------------------------------------------------
+
+        if nodeValue in self.nodes: return False
+
+        ### adding node to graph > incrementing size > returning true --------------------------------------------------
+
+        self.nodes[nodeValue] = list()
+        self.size += 1
+        return True
 
 ########################################################################################################################
 ### testing code
@@ -69,4 +92,18 @@ class Graph:
 
 print()
 my_graph = Graph()
-print("Init:", my_graph)
+print("Init:")
+print(my_graph)
+
+print("Add Node 0:", my_graph.add_node(nodeValue=0))
+print("Add Node 1:", my_graph.add_node(nodeValue=1))
+print("Add Node 2:", my_graph.add_node(nodeValue=2))
+print(my_graph)
+print("Add Node 3:", my_graph.add_node(nodeValue=3))
+print("Add Node 4:", my_graph.add_node(nodeValue=4))
+print("Add Node 5:", my_graph.add_node(nodeValue=5))
+print(my_graph)
+print("Add Node 6:", my_graph.add_node(nodeValue=6))
+print("Add Node ():", my_graph.add_node())
+print("Add Node 3:", my_graph.add_node(nodeValue=3))
+print(my_graph)

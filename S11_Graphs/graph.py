95345415de563f3bbe1937afac684c370b7c4099
Tibor Hajszan <thajszan@t-online.hu>
2025-01-31 12:38:16 +0100

    Lesson "Solution: Graph Implementation" : Coded delete_node() method.

--- S11_Graphs/graph.py
+++ S11_Graphs/graph.py
@@ -14,7 +14,7 @@ from typing import List, Dict, Any
 class Graph:
 
     ### constructor method #############################################################################################
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initializes a new empty Graph object.
 
@@ -64,13 +64,13 @@ class Graph:
 
         return graph_string
     
-    ### add node method ################################################################################################
-    def add_node(self, nodeValue:Any=None) -> bool:
+    ### insert node method #############################################################################################
+    def insert_node(self, insertValue:Any=None) -> bool:
         """
         Adds a new node containing the specified value to the Graph object.
 
         Args:
-        - nodeValue : Any | None, value of node to be added, defaults to None
+        - insertValue : Any | None, value of node to be added, defaults to None
 
         Returns:
         - bool, True = node is added | False = node is not added
@@ -78,22 +78,22 @@ class Graph:
 
         ### node already exists > returning false ----------------------------------------------------------------------
 
-        if nodeValue in self.nodes.keys(): return False
+        if insertValue in self.nodes.keys(): return False
 
         ### adding node to graph > incrementing size > returning true --------------------------------------------------
 
-        self.nodes[nodeValue] = list()
+        self.nodes[insertValue] = list()
         self.size += 1
         return True
     
-    ### add edge method ################################################################################################
-    def add_edge(self, nodeValue1:Any, nodeValue2:Any) -> bool:
+    ### insert edge method #############################################################################################
+    def insert_edge(self, edgeNode1:Any, edgeNode2:Any) -> bool:
         """
         Adds a new edge between specified nodes to the Graph object.
 
         Args:
-        - nodeValue1 : Any, value of first node in edge
-        - nodeValue2 : Any, value of second node in edge
+        - edgeNode1 : Any, value of first node in edge
+        - edgeNode2 : Any, value of second node in edge
 
         Returns:
         - bool, True = edge is added | False = edge is not added
@@ -101,16 +101,43 @@ class Graph:
 
         ### nonexistent nodes > returning false ------------------------------------------------------------------------
 
-        if nodeValue1 not in self.nodes.keys() or nodeValue2 not in self.nodes.keys(): return False
+        if edgeNode1 not in self.nodes.keys() or edgeNode2 not in self.nodes.keys(): return False
 
         ### edge already exists > returning false ----------------------------------------------------------------------
 
-        if nodeValue1 in self.nodes[nodeValue2]: return False
+        if edgeNode1 in self.nodes[edgeNode2]: return False
 
         ### adding edge to graph > returning true ----------------------------------------------------------------------
 
-        self.nodes[nodeValue1].append(nodeValue2)
-        self.nodes[nodeValue2].append(nodeValue1)
+        self.nodes[edgeNode1].append(edgeNode2)
+        self.nodes[edgeNode2].append(edgeNode1)
+        return True
+    
+    ### delete node method #############################################################################################
+    def delete_node(self, deleteValue:Any=None) -> bool:
+        """
+        Removes the node containing the specified value from the Graph object.
+
+        Args:
+        - deleteValue : Any | None, value of node to be deleted, defaults to None
+
+        Returns:
+        - bool, True = node is deleted | False = node is not deleted
+        """
+
+        ### nonexistent node > returning false -------------------------------------------------------------------------
+
+        if deleteValue not in self.nodes.keys(): return False
+
+        ### deleting edges ---------------------------------------------------------------------------------------------
+
+        for node in self.nodes[deleteValue]:
+            if deleteValue in self.nodes[node]: self.nodes[node].remove(deleteValue)
+
+        ### deleting node from graph > decrementing size > returning true ----------------------------------------------
+
+        del self.nodes[deleteValue]
+        self.size -= 1
         return True
 
 ########################################################################################################################
@@ -122,27 +149,32 @@ my_graph = Graph()
 print("Init:")
 print(my_graph)
 
-print("Add Node 0:", my_graph.add_node(nodeValue=0))
-print("Add Node 1:", my_graph.add_node(nodeValue=1))
-print("Add Node 2:", my_graph.add_node(nodeValue=2))
-print("Add Node 3:", my_graph.add_node(nodeValue=3))
-print("Add Node 4:", my_graph.add_node(nodeValue=4))
-print("Add Node 5:", my_graph.add_node(nodeValue=5))
+print("Add Node 0:", my_graph.insert_node(insertValue=0))
+print("Add Node 1:", my_graph.insert_node(insertValue=1))
+print("Add Node 2:", my_graph.insert_node(insertValue=2))
+print("Add Node 3:", my_graph.insert_node(insertValue=3))
+print("Add Node 4:", my_graph.insert_node(insertValue=4))
+print("Add Node 5:", my_graph.insert_node(insertValue=5))
 print(my_graph)
-print("Add Node 6:", my_graph.add_node(nodeValue=6))
-print("Add Node ():", my_graph.add_node())
-print("Add Node 3:", my_graph.add_node(nodeValue=3))
+print("Add Node 6:", my_graph.insert_node(insertValue=6))
+print("Add Node ():", my_graph.insert_node())
+print("Add Node 3:", my_graph.insert_node(insertValue=3))
 print(my_graph)
 
-print("Add Edge 3-1:", my_graph.add_edge(nodeValue1=3, nodeValue2=1))
-print("Add Edge 3-4:", my_graph.add_edge(nodeValue1=3, nodeValue2=4))
-print("Add Edge 4-2:", my_graph.add_edge(nodeValue1=4, nodeValue2=2))
-print("Add Edge 4-5:", my_graph.add_edge(nodeValue1=4, nodeValue2=5))
-print("Add Edge 1-2:", my_graph.add_edge(nodeValue1=1, nodeValue2=2))
-print("Add Edge 1-0:", my_graph.add_edge(nodeValue1=1, nodeValue2=0))
-print("Add Edge 0-2:", my_graph.add_edge(nodeValue1=0, nodeValue2=2))
-print("Add Edge 6-5:", my_graph.add_edge(nodeValue1=6, nodeValue2=5))
-print("Add Edge 5-6:", my_graph.add_edge(nodeValue1=5, nodeValue2=6))
-print("Add Edge 'xyz'-6:", my_graph.add_edge(nodeValue1="xyz", nodeValue2=6))
-print("Add Edge 6-20:", my_graph.add_edge(nodeValue1=6, nodeValue2=20))
+print("Add Edge 3-1:", my_graph.insert_edge(edgeNode1=3, edgeNode2=1))
+print("Add Edge 3-4:", my_graph.insert_edge(edgeNode1=3, edgeNode2=4))
+print("Add Edge 4-2:", my_graph.insert_edge(edgeNode1=4, edgeNode2=2))
+print("Add Edge 4-5:", my_graph.insert_edge(edgeNode1=4, edgeNode2=5))
+print("Add Edge 1-2:", my_graph.insert_edge(edgeNode1=1, edgeNode2=2))
+print("Add Edge 1-0:", my_graph.insert_edge(edgeNode1=1, edgeNode2=0))
+print("Add Edge 0-2:", my_graph.insert_edge(edgeNode1=0, edgeNode2=2))
+print("Add Edge 6-5:", my_graph.insert_edge(edgeNode1=6, edgeNode2=5))
+print("Add Edge 5-6:", my_graph.insert_edge(edgeNode1=5, edgeNode2=6))
+print("Add Edge 'xyz'-6:", my_graph.insert_edge(edgeNode1="xyz", edgeNode2=6))
+print("Add Edge 6-20:", my_graph.insert_edge(edgeNode1=6, edgeNode2=20))
+print(my_graph)
+
+print("Delete Node ():", my_graph.delete_node())
+print("Delete Node 20:", my_graph.delete_node(deleteValue=20))
+print("Delete Node 2:", my_graph.delete_node(deleteValue=2))
 print(my_graph)

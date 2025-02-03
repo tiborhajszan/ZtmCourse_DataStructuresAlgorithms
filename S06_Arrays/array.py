78274eec8054d7085afd634d8b62ba0fec28050e
Tibor Hajszan <thajszan@t-online.hu>
2025-02-03 13:33:09 +0100

    Lesson "Implementing An Array" : Coded delete() method.

--- S06_Arrays/array.py
+++ S06_Arrays/array.py
@@ -60,7 +60,10 @@ class Array:
         Validates the provided array index.
 
         Args:
-        - checkIndex : int, array index to check
+        - checkIndex : int | None, array index to check
+
+        Raises:
+        - system exit, if array index is invalid
 
         Returns:
         - None
@@ -146,50 +149,37 @@ class Array:
         self._checkIndex(checkIndex=getIndex)
         return self.data[getIndex]
     
-    ### left shift method ##############################################################################################
-    def _shiftLeft(self, leftIndex=int()) -> None:
-        """
-        Moves items one position to the left, starting from the specified position up to the end of the array.
-        It is used internally to fill the deleted position in the array.
-
-        Args:
-        - leftIndex: int, position from where items are shifted, defaults to 0
-        """
-
-        # iterating through items to be shifted
-        for index in range(leftIndex, self.length - 1):
-            # shifting item
-            self.data[index] = self.data[index+1]
-        # deleting last item
-        del self.data[self.length - 1]
-        # returning none
-        return
-    
     ### delete method ##################################################################################################
-    def delete(self, deleteIndex=None) -> int:
+    def delete(self, deleteIndex:int=None) -> Any:
         """
-        Deletes the item at the specified position in the array.
-        If the position is not specified, the last item is deleted.
+        Removes an item from the Array object at the specified position.
 
         Args:
-        - deleteIndex: int|None, position where item is deleted, defaults to None
+        - deleteIndex: int | None, index of item to be deleted, defaults to None
 
         Returns:
-        - int, new length of array
+        - deleted_item: Any, value of deleted item
         """
 
-        # index not specified > assigning last index
+        ### validating delete index ------------------------------------------------------------------------------------
+
+        self._checkIndex(checkIndex=deleteIndex)
         if deleteIndex == None: deleteIndex = self.length - 1
-        #  index specified > fixing out of range index
-        else: deleteIndex = self._checkIndex(checkIndex=deleteIndex)
-        # clering item
-        self.data[deleteIndex] = None
-        # shifting items left
-        self._shiftLeft(leftIndex=deleteIndex)
-        # updating array length
+
+        ### recording deleted item -------------------------------------------------------------------------------------
+
+        deleted_item: Any = self.data[deleteIndex]
+
+        ### shifting items left ----------------------------------------------------------------------------------------
+
+        for index in range(deleteIndex, self.length-1):
+            self.data[index] = self.data[index+1]
+
+        ### deleting item > updating array length > returning deleted value --------------------------------------------
+
+        del self.data[self.length-1]
         self.length -= 1
-        # returning new array length
-        return self.length
+        return deleted_item
 
     ### pop method #####################################################################################################
     def pop(self) -> int:
@@ -210,32 +200,36 @@ class Array:
 my_array = Array()
 print("\nInit:", my_array, "\n")
 
-print("Check(None):"); my_array._checkIndex(checkIndex=None)
+# print("Check(None):"); my_array._checkIndex(checkIndex=None)
 # print("Check('0'):"); my_array._checkIndex(checkIndex='0')
 # print("Check(-1):"); my_array._checkIndex(checkIndex=-1)
 # print("Check(0):"); my_array._checkIndex(checkIndex=0)
-print("Index OK...\n")
+# print("Index OK...\n")
 
-my_array.insert()
-print("Insert():", my_array)
-my_array.insert(insertItem="Tail")
-print("Insert('Tail'):", my_array)
-my_array.insert(insertIndex=0, insertItem="there")
-print("Insert(0,'there'):", my_array)
-my_array.insert(insertIndex=0, insertItem="Hey")
-print("Insert(0,'Hey'):", my_array)
-my_array.insert(insertIndex=2, insertItem="sweet")
-print("Insert(2,'sweet'):", my_array, "\n")
+print("Insert():", my_array.insert(), my_array)
+print("Insert('Tail'):", my_array.insert(insertItem="Tail"), my_array)
+print("Insert(0,'there'):", my_array.insert(insertIndex=0, insertItem="there"), my_array)
+print("Insert(0,'Hello'):", my_array.insert(insertIndex=0, insertItem="Hello"), my_array)
+print("Insert(2,'sweet'):", my_array.insert(insertIndex=2, insertItem="sweet"), my_array, "\n")
+# print("Insert('test','test')", my_array.insert(insertIndex="test", insertItem="test"), my_array, "\n")
 
-my_array.push(pushItem=16)
-print("Push(16):", my_array)
-my_array.push(pushItem='!')
-print("Push('!'):", my_array, "\n")
+print("Push(16):", my_array.push(pushItem=16), my_array)
+print("Push('?'):", my_array.push(pushItem="?"), my_array, "\n")
 
+my_array[6] = "!"
+print("Set[6] '!':", my_array)
+my_array[0] = "Hey"
+print("Set[0] 'Hey':", my_array)
 my_array[3] = "Joe"
-print("Set[3] 'Joe'", my_array, "\n")
-# my_array[15] = "Hi"
+print("Set[3] 'Joe':", my_array, "\n")
+# my_array[-3] = "Hi"
 
-print("Get[1]:", my_array[1])
-print("Get[3]:", my_array[3])
-print("Get[15]:", my_array[15], "\n")
+print("Get[6]:", my_array[6])
+print("Get[0]:", my_array[0])
+print("Get[3]:", my_array[3], "\n")
+# print("Get[15]:", my_array[15], "\n")
+
+print("Delete():", my_array.delete(), my_array)
+print("Delete(0):", my_array.delete(deleteIndex=0), my_array)
+print("Delete(3):", my_array.delete(deleteIndex=3), my_array, "\n")
+print("Delete(15):", my_array.delete(deleteIndex=15), my_array, "\n")

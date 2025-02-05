ec48a20afc54860cf23a6be65bed8f518f995dcf
Tibor Hajszan <thajszan@t-online.hu>
2025-02-05 12:31:27 +0100

    Lesson "Implementing An Array" : Updated _checkIndex() and insert() methods.

--- S06_Arrays/array.py
+++ S06_Arrays/array.py
@@ -60,36 +60,37 @@ class Array:
         Validates the provided array index.
 
         Args:
-        - checkIndex : int | None, array index to check
+        - checkIndex : int, array index to check
 
         Returns:
         - bool, True = valid index | False = invalid index
         """
 
-        if checkIndex is None or (type(checkIndex) is int and 0 <= checkIndex < self.length): return True
-        return False
+        if type(checkIndex) is not int or checkIndex < -1 or self.length <= checkIndex: return False
+        return True
     
     ### insert method ##################################################################################################
-    def insert(self, insertIndex:int=None, insertItem:Any=None) -> str:
+    def insert(self, insertIndex:int=-1, insertItem:Any=None) -> None:
         """
         Adds a new item to the Array object at the specified position.
 
         Args:
-        - insertIndex : int | None, position where item is inserted, defaults to None
-        - insertItem : Any | None, item to be inserted, defaults to None
+        - insertIndex : int, position where item is inserted, defaults to -1
+        - insertItem : Any, item to be inserted, defaults to None
 
         Returns:
-        - str, "OK" if insert successful | str, result of _checkIndex() method if invalid index
+        - None
         """
 
-        ### invalid insert index > returning error message -------------------------------------------------------------
+        ### invalid insert index > printing error message > returning none ---------------------------------------------
 
-        check_result: str = self._checkIndex(checkIndex=insertIndex)
-        if 0 < len(check_result): return check_result
+        if not self._checkIndex(checkIndex=insertIndex):
+            print(f"Invalid index: {[insertIndex]}")
+            return
 
         ### insert index not provided > appending to end of array ------------------------------------------------------
 
-        if insertIndex is None: insertIndex = self.length
+        if insertIndex == -1: insertIndex = self.length
 
         ### insert position occupied > shifting items right ------------------------------------------------------------
 
@@ -97,11 +98,11 @@ class Array:
             for index in range(self.length, insertIndex, -1):
                 self.data[index] = self.data[index-1]
         
-        ### inserting item > updating array length > returning OK ------------------------------------------------------
+        ### inserting item > updating array length > returning none ----------------------------------------------------
 
         self.data[insertIndex] = insertItem
         self.length += 1
-        return "OK"
+        return
     
     ### push method ####################################################################################################
     def push(self, pushItem:Any=None) -> str:
@@ -229,14 +230,16 @@ print("Check(None):", my_array._checkIndex(checkIndex=None))
 print("Check('0'):", my_array._checkIndex(checkIndex="0"))
 print("Check(-1):", my_array._checkIndex(checkIndex=-1))
 print("Check(0):", my_array._checkIndex(checkIndex=0), "\n")
-sys.exit()
 
-print("Insert():", my_array.insert(), my_array)
-print("Insert('Append'):", my_array.insert(insertItem="Append"), my_array)
-print("Insert(0,'there'):", my_array.insert(insertIndex=0, insertItem="there"), my_array)
-print("Insert(0,'Hello'):", my_array.insert(insertIndex=0, insertItem="Hello"), my_array)
-print("Insert(2,'sweet'):", my_array.insert(insertIndex=2, insertItem="sweet"), my_array)
-print("Insert('test','test')", my_array.insert(insertIndex="test", insertItem="test"), my_array, "\n")
+### testing insert method ----------------------------------------------------------------------------------------------
+
+print("Insert():", end=" "); my_array.insert(); print(my_array)
+print("Insert('Append'):", end=" "); my_array.insert(insertItem="Append"); print(my_array)
+print("Insert(0,'there'):", end=" "); my_array.insert(insertIndex=0, insertItem="there"); print(my_array)
+print("Insert(0,'Hello'):", end=" "); my_array.insert(insertIndex=0, insertItem="Hello"); print(my_array)
+print("Insert(2,'sweet'):", end=" "); my_array.insert(insertIndex=2, insertItem="sweet"); print(my_array)
+print("Insert('test','test'):", end=" "); my_array.insert(insertIndex="test", insertItem="test"); print(my_array, "\n")
+sys.exit(0)
 
 print("Push(16):", my_array.push(pushItem=16), my_array)
 print("Push('?'):", my_array.push(pushItem="?"), my_array, "\n")

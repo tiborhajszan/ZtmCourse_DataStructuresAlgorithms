2ec37dd3ac1408425eb054ae0e2891328f728a77
Tibor Hajszan <thajszan@t-online.hu>
2025-10-02 14:20:58 +0200

    >64 Lesson "Solution: Sorting Interview" : completed.

--- S13_Sorting/sorting_exercise.py
+++ S13_Sorting/sorting_exercise.py
@@ -1,15 +1,29 @@
-### 1 - Sort 10 schools around your house by distance: Insertion Sort
+########################################################################################################################
+### Data Structures and Algorithms
+### Section 13 | Sorting Exercise
+########################################################################################################################
 
-### 2 - eBay sorts listings by the current Bid amount: Radix or Counting Sort
+### 1 - Sort 10 schools around your house by distance:
+# Insertion Sort (good for small datasets)
 
-### 3 - Sport scores on ESPN: Quick Sort
+### 2 - Sorting eBay listings by the current bid amount:
+# Counting or Radix Sort (good for limited number/range of integers)
 
-### 4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge Sort
+### 3 - Sorting sport scores on ESPN:
+# Quick Sort (good for large/random datasets, main issue is memory)
 
-### 5 - Almost sorted Udemy review data needs to update and add 2 new reviews: Insertion Sort
+### 4 - Sorting a massive database (does not fit into memory) of past year's user data:
+# Merge Sort (good for large/random datasets, main issue is performance)
 
-### 6 - Temperature Records for the past 50 years in Canada: Radix or Counting Sort
+### 5 - Adding 2 new reviews and updating a presorted list of Udemy reviews:
+# Insertion Sort (good for adding small datasets to a presorted list)
 
-### 7 - Large user name database needs to be sorted. Data is very random: Merge Sort
+### 6 - Sorting temperature records for the past 50 years in Canada:
+# Integers: Counting or Radix Sort (good for limited number/range of integers)
+# Decimals: Quick Sort (good for large/random datasets, main issue is memory)
 
-### 8 - You want to teach sorting for the first time: Bubble Sort, Selection Sort
+### 7 - Sorting a large user database. Data is very random:
+# Quick Sort (good for large/random datasets, main issue is memory)
+
+### 8 - Teaching sorting algorithms for the first time:
+# Bubble Sort, Selection Sort (simple, easy to understand algorithms)

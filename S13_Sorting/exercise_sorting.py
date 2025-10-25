ef6d2ac73872940146f296909364c48af4a57f5f
Tibor Hajszan <thajszan@t-online.hu>
2025-10-25 15:25:08 +0200

    67> Lesson "Solution: BFS vs DFS" : completed.

--- S13_Sorting/exercise_sorting.py
+++ S13_Sorting/exercise_sorting.py
@@ -3,27 +3,27 @@
 ### Section 13 | Sorting Exercise
 ########################################################################################################################
 
-### 1 - Sort 10 schools around your house by distance:
+### 1 - Sorting 10 schools around your house by distance:
 # Insertion Sort (good for small datasets)
 
 ### 2 - Sorting eBay listings by the current bid amount:
-# Counting or Radix Sort (good for limited number/range of integers)
+# Counting Sort or Radix Sort (good for limited number/range of integers)
 
 ### 3 - Sorting sport scores on ESPN:
-# Quick Sort (good for large/random datasets, main issue is memory)
+# Quick Sort (good for large/random datasets, main concern is memory)
 
-### 4 - Sorting a massive database (does not fit into memory) of past year's user data:
-# Merge Sort (good for large/random datasets, main issue is performance)
+### 4 - Sorting a massive database (does not fit into memory) of user data from last year:
+# Merge Sort (good for large/random datasets, main concern is performance)
 
-### 5 - Adding 2 new reviews and updating a presorted list of Udemy reviews:
-# Insertion Sort (good for adding small datasets to a presorted list)
+### 5 - Adding 2 new reviews to update a presorted list of Udemy reviews:
+# Insertion Sort (good for adding a few items to a presorted dataset)
 
 ### 6 - Sorting temperature records for the past 50 years in Canada:
-# Integers: Counting or Radix Sort (good for limited number/range of integers)
-# Decimals: Quick Sort (good for large/random datasets, main issue is memory)
+# Integers: Counting Sort or Radix Sort (good for limited number/range of integers)
+# Decimals: Quick Sort (good for large/random datasets, main concern is memory)
 
 ### 7 - Sorting a large user database. Data is very random:
-# Quick Sort (good for large/random datasets, main issue is memory)
+# Quick Sort (good for large/random datasets, main concern is memory)
 
 ### 8 - Teaching sorting algorithms for the first time:
-# Bubble Sort, Selection Sort (simple, easy to understand algorithms)
+# Bubble Sort or Selection Sort (simple, easy to understand algorithms)
--- S14_Searching/exercise_traversing.py
+++ S14_Searching/exercise_traversing.py
@@ -1,16 +1,16 @@
 ########################################################################################################################
 ### Data Structures and Algorithms
-### Section 13 | Traversing Exercise
+### Section 14 | Traversing Exercise
 ########################################################################################################################
 
 ### If you know a solution is not far from the root of the tree: BFS
 
-### If the tree is very deep and solutions are rare: DFS
+### If solutions are frequent and located deep in the tree: DFS
 
-### If the tree is very wide: BFS?
-
-### If solutions are frequent but located deep in the tree: DFS
+### Finding the shortest path: BFS
 
 ### Determining whether a path exists between two nodes: DFS
 
-### Finding the shortest path: BFS
+### If the tree is very deep and solutions are rare: BFS (DFS would take too much time)
+
+### If the tree is very wide: DFS (BFS would need too much memory)

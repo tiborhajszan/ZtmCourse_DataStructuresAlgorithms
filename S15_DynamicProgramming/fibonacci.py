9ff2bd9b3bac7dbf1c5927dbf8c2b509ad41a977
Tibor Hajszan <thajszan@t-online.hu>
2025-11-27 15:42:34 +0100

    74> Lesson "Dynamic Programming Review" : completed.

--- S15_DynamicProgramming/fibonacci.py
+++ S15_DynamicProgramming/fibonacci.py
@@ -1,9 +1,9 @@
 ########################################################################################################################
 ### Data Structures And Algorithms
-### Section 15 | Dynamic Fibonacci Function
+### Section 15 | Fibonacci Function | Dynamic, BottomUp Implementations
 ########################################################################################################################
 
-from typing import Dict
+from typing import List, Dict
 calculations: int = 0
 
 ### dynamic fibonacci function #########################################################################################
@@ -48,19 +48,59 @@ def fibonacci_dynamic():
 
     return closure
 
+### fibonacci bottom up dynamic function ###############################################################################
+    
+def fibonacci_bottomUp(fibonacciIndex:int=0) -> int:
+    """
+    Calculates a value in the Fibonacci sequence using bottom up dynamic programming.
+
+    Args:
+    - fibonacciIndex : int, index of Fibonacci sequence, defaults to 0
+    Returns:
+    - int, specified Fibonacci value | -1, Fibonacci index is invalid
+    """
+
+    ### invalid argument > returning -1 > counter init -----------------------------------------------------------------
+
+    if type(fibonacciIndex) is not int or fibonacciIndex < 0: return -1
+    global calculations; calculations = 0
+
+    ### fibonacci index 0,1 > returning fibonacci index ----------------------------------------------------------------
+
+    if fibonacciIndex < 2: return fibonacciIndex
+
+    ### calculating fibonacci sequence ---------------------------------------------------------------------------------
+
+    fibonacci_sequence: List[int] = [0, 1]
+    current_index: int = 2
+    while current_index <= fibonacciIndex:
+        fibonacci_sequence.append(fibonacci_sequence[current_index-2] + fibonacci_sequence[current_index-1])
+        calculations += 1; current_index += 1
+    
+    ### returning fibonacci value --------------------------------------------------------------------------------------
+
+    return fibonacci_sequence.pop()
+
 ########################################################################################################################
 ### testing code
 ########################################################################################################################
 
 print()
+print("Fibonacci Dynamic Programming")
+print()
 
-print("Fibonacci()", fibonacci_dynamic()())
-print("Fibonacci('test')", fibonacci_dynamic()(fibonacciIndex="test"))
-print("Fibonacci(-1)", fibonacci_dynamic()(fibonacciIndex=-1))
+print("Dynamic()", fibonacci_dynamic()())
+print("Dynamic('test')", fibonacci_dynamic()(fibonacciIndex="test"))
+print("Dynamic(-1)", fibonacci_dynamic()(fibonacciIndex=-1))
+print("Dynamic(300)", fibonacci_dynamic()(fibonacciIndex=300))
+print("Calculations: ", calculations)
 
 print()
 
-print("Fibonacci(300)", fibonacci_dynamic()(fibonacciIndex=300))
+print("BottomUp()", fibonacci_bottomUp())
+print("BottomUp('test')", fibonacci_bottomUp(fibonacciIndex="test"))
+print("BottomUp(-1)", fibonacci_bottomUp(fibonacciIndex=-1))
+print("BottomUp(300)", fibonacci_bottomUp(fibonacciIndex=300))
 print("Calculations: ", calculations)
 
 print()
